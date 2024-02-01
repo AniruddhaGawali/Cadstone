071a559127c25889d5fe4cb232fbe1640d1d55e6
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-02-01 15:27:56 +0530

    Refactor dice display logic

--- diceSim.py
+++ diceSim.py
@@ -1,52 +1,61 @@
 import random
 
-x = "n"
+x = "y"
 
 #Generate a random number between 1 and 6 (inclusive)
 
 x = input("Press y to roll again and n to exit: ")
 while x=="y":
-    no = random.randint(0,6)
+    no = random.randint(1,6)
 
     if no == 1:
-        print("[-----]")
-        print("[-----]")
-        print("[--0--]")
-        print("[-----]")
-        print("[-----]")
+        for i in range(5):
+            if i == 2:
+                print("[--0--]")
+            else:
+                print("[-----]")
+
+
         
     if no == 2:
-        print("[-----]")
-        print("[--0--]")
-        print("[-----]")
-        print("[--0--]")
-        print("[-----]")
-        break
-    if no == 3:
-        print("[-----]")
-        print("[--0--]")
-        print("[--0--]")
-        print("[--0--]")
-        print("[-----]")
-    if no == 4:
-        print("[-----]")
-        print("[0---0]")
-        print("[-----]")
-        print("[0---0]")
-        print("[-----]")
-    if no ==5:
-        print("[-----]")
-        print("[0---0]")
-        print("[--0--]")
-        print("[0---0]")
-        print("[-----]")
-    if no == 6:
-        print("[-----]")
-        print("[0---0]")
-        print("[0---0]")
-        print("[0---0]")
-        print("[-----]")
+        for i in range(5):
+            if i==1 or i==3:
+                print("[--0--]")
+            else:
+                print("[-----]")
 
+
+    if no == 3:
+        for i in range(5):
+            if i==1 or i==2 or i==3:
+                print("[--0--]")
+            else:
+                print("[-----]")
+                
+    if no == 4:
+        for i in range(5):
+            if i==1 or i==3:
+                print("[0---0]")
+            else:
+                print("[-----]")
+                
+
+    if no ==5:
+        for i in range(5):
+            if i==1 or i==3:
+                print("[0---0]")
+            elif i==2:
+                print("[--0--]")
+            else:
+                print("[-----]")
+
+    if no == 6:
+        for i in range(5):
+            if i==1 or i==3 or i==2:
+                print("[0---0]")
+            else:
+                print("[-----]")
+                
     x = input("Press y to roll again and n to exit: ")
 
 
