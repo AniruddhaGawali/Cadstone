cb28a1291c3eef833b7dbdf3e882be28f8d6b269
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-02-14 15:26:48 +0530

    Add alarm clock functionality to the project

---
+++ alarmClock.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+
+def set_alarm(alarm_time):
+    while True:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        if current_time == alarm_time:
+            print("Wake up!")
+            break
+        time.sleep(1)
+
+def main():
+    print("Enter the time for the alarm (format: HH:MM:SS)")
+    alarm_time = input(">> ")
+    try:
+        datetime.datetime.strptime(alarm_time, "%H:%M:%S")
+    except ValueError:
+        print("Invalid time format. Please use HH:MM:SS.")
+        return
+    set_alarm(alarm_time)
+
+if __name__ == "__main__":
+    main()
